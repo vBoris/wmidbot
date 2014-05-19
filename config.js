96620258c3470c5c6a69e17e88799847bd55ea11
Kazakov <alekseykass@gmail.com>
2014-05-19 12:22:01 +0200

    admin commit

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ Config({
 	server:false,
 	captcha:"//kptch1.wmid.com.ua/get.php"
 });
+$('head').append('<link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/gpmmllknahfkgeonmlhdaemofpfkboib">');
