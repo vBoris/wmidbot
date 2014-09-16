df9c8b4b3eb89507b387ca8e8a2e5b2c7f087a5c
Kazakov <alekseykass@gmail.com>
2014-09-16 19:54:02 +0200

    admin commit

--- config.js
+++ config.js
@@ -2,4 +2,3 @@ Config({
 	server:false,
 	captcha:"//kptch1.wmid.com.ua/get.php"
 });
-$(function(){ $('head').append('<link rel="chrome-webstore-wmidbot" href="https://chrome.google.com/webstore/detail/gpmmllknahfkgeonmlhdaemofpfkboib">');});
