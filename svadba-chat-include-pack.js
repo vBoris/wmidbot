73b54f56fbf532441768c4ee1758ffaa32e15cf0
Kazakov <alekseykass@gmail.com>
2014-09-25 10:34:01 +0200

    admin commit

--- svadba-chat-include-pack.js
+++ svadba-chat-include-pack.js
@@ -12,11 +12,15 @@ $('head').append('<style>#chat_act .message {height:11px!important;} #online-opp
 $('#translate input[type="submit"]').css({ width: '112px', height: '27px', background: '#26ade4', textIndent: '0', lineHeight: 'auto', marginRight: '20px', fontWeight: 'bold', color: '#fff', borderRadius: '10px'}).val('WMID Translate');
 $('#translate .popup').hide();*/
 
-function translb(id){
-	
+function translb(sel){
+	$(sel).html('<a href="javascript:void(0)" id="wmid_trans" style="width:112px; height: 27px; background: #26ade4; textIndent: 0; lineHeight: auto; marginRight: 20px; fontWeight: bold; color: #fff; borderRadius: 10px">WMID Translate</a>')
 }
-translb(id);
-
+translb('#translate');
+$(function(){
+	$('#wmid_trans').click(function(){
+		
+	});
+});
 
 function strt(request){
 			var postlist = [];
