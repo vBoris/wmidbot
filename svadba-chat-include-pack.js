ac8c683e4f89cbccf7b52d42257fd29807ccc2f0
Kazakov <alekseykass@gmail.com>
2014-09-25 11:10:46 +0200

    admin commit

--- svadba-chat-include-pack.js
+++ svadba-chat-include-pack.js
@@ -12,7 +12,7 @@ $('head').append('<style>#chat_act .message {height:11px!important;} #online-opp
 $('#translate input[type="submit"]').css({ width: '112px', height: '27px', background: '#26ade4', textIndent: '0', lineHeight: 'auto', marginRight: '20px', fontWeight: 'bold', color: '#fff', borderRadius: '10px'}).val('WMID Translate');
 $('#translate .popup').hide();*/
 function translb(sel){
-	$(sel).after('<a href="javascript:void(0)" id="wmid_trans" style="width:112px; height: 27px; background: #26ade4; text-indent: 0; line-height: auto; margin-right: 20px; font-weight: bold; color: #fff; border-radius: 10px; float:left;">WMID Translate</a>');
+	$(sel).after('<a href="javascript:void(0)" id="wmid_trans" style="width:112px; height: 27px; background: #26ade4; text-indent: 0; line-height: auto; margin-right: 20px; font-weight: bold; color: #fff; border-radius: 10px; float:left;text-decoration: none;font-size: 14px;text-align: center;">WMID Translate</a>');
 	$('#wmid_trans').click(function(){
 		$.getJSON('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20140925T082047Z.5055d7e52197b592.bda3ad29dbb6a6aa6d19098d6e9748aca550221e&text='+$('#message').val()+'&lang=en',function(s){
 			console.log(s.text);
